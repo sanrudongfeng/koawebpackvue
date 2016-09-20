d7d41940ebe1815fef2244dd5f4eadf2d69adbe4
sanrudongfeng <15287025483@sina.cn>
2016-09-20 17:24:13 +0800

    第一次提交

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import koa from 'koa';
 import crsf from 'crsf';
+import router from 'koa-router';
 
 let app = new koa();
-app.use();
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 app.listen(3000);
