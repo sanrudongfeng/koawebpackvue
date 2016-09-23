a39e5453d8f3664bc857182688827a27e82f2824
sanrudongfeng <15287025483@sina.cn>
2016-09-23 16:20:28 +0800

    add some files

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 import koa from 'koa';
+import convert from 'koa-convert';
+import bodyParser from 'koa-bodyparser';
+import statics from 'koa-static';
+import views from 'koa-views';
 import router from 'koa-router';
 
+
 let app = new koa();
+
+//三者有一定顺序
+app.use(views(__dirname + '/dist', {
+    map: {
+        html: 'underscore',
+        ejs:'ejs'
+    }
+}));
+
+app.use(bodyParser());
+
+app.use(convert(statics('./public')));
+
 app
     .use(router.routes())
     .use(router.allowedMethods());
---
+++ test/gutest.js
@@ -0,0 +1,19 @@
+/*var Keygrip = require('keygrip');
+console.log('Keygrip',Keygrip);
+var keys1 = Keygrip(["SEKRIT2", "SEKRIT1"]);
+console.log('key1',keys1.sign('1'));
+var keys2 = Keygrip(["SEKRIT2"]);
+console.log('key2',keys2.sign('1'));
+ console.log(keys1.verify("1"));*/
+
+
+
+
+
+var o ={
+  _a:1,
+    set a(a){
+        this.a = a;
+    }
+};
+console.log('o._a',o._a);
