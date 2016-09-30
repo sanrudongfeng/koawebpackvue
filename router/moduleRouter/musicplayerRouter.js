240c5cc0bf06f49776073c3896b2724ce1cbbe64
sanrudongfeng <15287025483@sina.cn>
2016-09-30 18:01:25 +0800

    html page

--- router/moduleRouter/musicplayerRouter.js
+++ router/moduleRouter/musicplayerRouter.js
@@ -1,10 +1,14 @@
-
+import fs from 'fs';
 
 function musicPlayer(router){
     router.prefix('/musicPlayer');
     router.get('/',async (ctx,next) =>{
-        console
         await ctx.render('musicplayer.html',{});
+    });
+    router.post('/list',async (ctx,next) =>{
+        console.log(`${__dirname}/media`);
+         let musicList = fs.readdirSync(`${__dirname}/media`);
+         return ctx.body = {code:0,msg:'获取音乐列表成功',data:musicList};
     });
 }
 export default musicPlayer
